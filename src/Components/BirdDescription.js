c287a4ed6a790ca304a65b3b9c29b6fd36a83374
maria larchenko <mashalarchenko16@gmail.com>
2020-02-09 01:16:40 +0300

    feat: add proptypes validation

--- src/Components/BirdDescription.js
+++ src/Components/BirdDescription.js
@@ -1,10 +1,13 @@
+/* eslint-disable react/require-default-props */
 import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 import Context from '../context';
 import Player from './AudioPlayer';
+import birdState from './birdState';
 
 const useStyle = makeStyles(() => ({
   sectionWrapper: {
@@ -18,30 +21,41 @@ const useStyle = makeStyles(() => ({
   },
 }));
 
-
-export default function BirdDescription() {
+function BirdDescription({ levelCount, selectedBirdId, startQ }) {
   const styles = useStyle();
   const { birdData, image } = useContext(Context);
+  const selectedBird = birdState[0][levelCount][selectedBirdId - 1];
+  console.log(birdData, image);
 
   return (
     <Card className={styles.sectionWrapper}>
-      <CardContent>
-        <img src={image} alt="unknownbird" className={styles.image} />
-        <Typography variant="h4" component="h4" className={styles.typog}>
-          {birdData.en}
-        </Typography>
-        <Typography variant="h5" component="h5" className={styles.typog}>
-          {birdData.gen + birdData.sp}
-        </Typography>
-        <Typography variant="h5" component="h5" className={styles.typog}>
-          Location
-          {birdData.loc}
-        </Typography>
-        <Player link={birdData.file} />
-        <p className={styles.typog}>
-          Description
-        </p>
-      </CardContent>
+      {startQ ? (
+        <CardContent>
+          <img src={image} alt="unknownbird" className={styles.image} />
+          <Typography variant="h4" component="h4" className={styles.typog}>
+            {birdData.en}
+          </Typography>
+          <Typography variant="h5" component="h5" className={styles.typog}>
+            {selectedBird.species}
+          </Typography>
+          <Typography variant="h5" component="h5" className={styles.typog}>
+            Location
+            {birdData.loc}
+          </Typography>
+          <Player link={birdData.file} />
+          <p className={styles.typog}>
+            {selectedBird.description}
+          </p>
+        </CardContent>
+      ) : 'no data'}
     </Card>
   );
 }
+
+BirdDescription.propTypes = {
+  levelCount: PropTypes.number.isRequired,
+  selectedBirdId: PropTypes.number,
+  startQ: PropTypes.bool.isRequired,
+};
+
+export default BirdDescription;
--- src/Components/BirdList.js
+++ src/Components/BirdList.js
@@ -1,10 +1,10 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import List from '@material-ui/core/List';
 // import Context from '../context';
+import PropTypes from 'prop-types';
 import BirdItem from './BirdItem';
 import birdState from './birdState';
 
@@ -15,7 +15,7 @@ const useStyle = makeStyles(() => ({
   },
 }));
 
-export default function BirdList({
+function BirdList({
   levelCount,
   setImage, setData, setAnswerState, isAnswerState,
 }) {
@@ -33,6 +33,7 @@ export default function BirdList({
               name={bird.name}
               key={bird.id}
               species={bird.species}
+              id={bird.id}
               setImage={setImage}
               setData={setData}
               setAnswerState={setAnswerState}
@@ -44,3 +45,18 @@ export default function BirdList({
     </Card>
   );
 }
+
+BirdList.propTypes = {
+  levelCount: PropTypes.number.isRequired,
+  setImage: PropTypes.func.isRequired,
+  setData: PropTypes.func.isRequired,
+  setAnswerState: PropTypes.func.isRequired,
+  isAnswerState: PropTypes.shape({
+    noRightAnswer: PropTypes.bool,
+    startQ: PropTypes.bool,
+    selectedBird: PropTypes.string,
+    birdId: PropTypes.number,
+  }).isRequired,
+};
+
+export default BirdList;
